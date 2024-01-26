27d8f4d400dd2c17831085c6af56e68d0488b96a
huayezhan <huayezhan@protonmail.com>
2024-01-26 11:39:34 +0800

    all-completed

--- src/about.jsx
+++ src/about.jsx
@@ -1,4 +1,5 @@
 import photo from './photo.jpeg'
+import pdf from "./resume.pdf"
 
 export default function About() {
     return (
@@ -6,7 +7,8 @@ export default function About() {
         <p><b>Image of me:</b> <img src={photo}  height = "200px"/></p>
         <p><b>Legal name:</b> Huaye Zhan</p>
         <p><b>English name:</b> Howard Zhan</p>
-        <p><b>My resume:</b> https://www.zhylr.top/wp-content/uploads/2024/01/resume-english-version.pdf</p>
+        <p><b>My resume:</b> </p>
+        <p><embed src={pdf} width="800px" height="1200px" /></p>
         <p>I am Howard, from Guangzhou, China.</p> 
         <p>I am a full-time software developer, AI engineer, Robotic engineer.</p>
       </>
